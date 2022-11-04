6c4bf6991fadb07f1459a0bebbf69f1582a6a96b
linsk1998 <h@linsk.us>
2022-11-04 15:04:38 +0800

    style\delete import ""

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ var pluginutils = require('@rollup/pluginutils');
 var MagicString = require('magic-string');
 //var kebabCase = require('kebab-case');
 
-function noChange(name){
+function noChange(name) {
 	return name;
 }
 function plugin(options) {
 	if (!options) { throw new Error('Missing options'); }
-	if(!options.libraryName){ throw new Error('Missing options.libraryName'); }
+	if (!options.libraryName) { throw new Error('Missing options.libraryName'); }
 	//options.libraryDirectory=options.libraryDirectory || "lib";
-	options.namedComponent=options.namedComponent || noChange;
-	options.customName=options.customName || function(name){
-		if(options.libraryDirectory){
+	options.namedComponent = options.namedComponent || noChange;
+	options.customName = options.customName || function(name) {
+		if (options.libraryDirectory) {
 			return `${options.libraryName}/${options.libraryDirectory}/${options.namedComponent(name)}`;
 		}
 		return `${options.libraryName}/${options.namedComponent(name)}`;
 	};
 	//options.exportName=undefined;
-	if(options.style===true){
-		options.style="style";
+	var style = options.style;
+	if (style === true) {
+		style = "style";
+	}
+	if (typeof style !== "function") {
+		options.style = function(name) {
+			return `${name}/${style}`;
+		};
 	}
 	var filter = pluginutils.createFilter(options.include, options.exclude);
 
@@ -41,35 +47,35 @@ function plugin(options) {
 				return null;
 			}
 			var magicString = new MagicString(code);
-			var hasChanged=false;
+			var hasChanged = false;
 			//console.log(JSON.stringify(ast, null, 1));
-			ast.body.forEach(function (node) {
+			ast.body.forEach(function(node) {
 				if (node.type === 'ImportDeclaration') {
-					if(node.source.value==options.libraryName){
-						var imports=[];
-						node.specifiers.forEach(function (specifier) {
-							var importedName=specifier.imported?specifier.imported.name:"default";
-							var localName=specifier.local.name;
-							var modName=options.customName(importedName);
-							var exportName=options.exportName || importedName;
-							if(exportName==="default"){
+					if (node.source.value == options.libraryName) {
+						var imports = [];
+						node.specifiers.forEach(function(specifier) {
+							var importedName = specifier.imported ? specifier.imported.name : "default";
+							var localName = specifier.local.name;
+							var modName = options.customName(importedName);
+							var exportName = options.exportName || importedName;
+							if (exportName === "default") {
 								imports.push(`import ${localName} from ${JSON.stringify(modName)};`);
-							}else if(exportName===localName){
+							} else if (exportName === localName) {
 								imports.push(`import {${exportName}} from ${JSON.stringify(modName)};`);
-							}else if(exportName==="*"){
+							} else if (exportName === "*") {
 								imports.push(`import ${exportName} as ${localName} from ${JSON.stringify(modName)};`);
-							}else{
+							} else {
 								imports.push(`import {${exportName} as ${localName}} from ${JSON.stringify(modName)};`);
 							}
-							if(options.style){
-								imports.push(`import ${JSON.stringify(`${modName}/${options.style}`)};`);
+							if (options.style) {
+								var styleName = options.style(modName);
+								if (styleName) {
+									imports.push(`import ${JSON.stringify(styleName)};`);
+								}
 							}
 						});
-						if(imports.length){
-							//console.log(imports);
-							hasChanged=true;
-							magicString.overwrite(node.start, node.end, imports.join(''));
-						}
+						hasChanged = true;
+						magicString.overwrite(node.start, node.end, imports.join(''));
 					}
 				}
 			});
